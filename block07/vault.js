312008a0a4c40f90206d476673772cfcaede08e2
jcatekista <126266848+jcatekista@users.noreply.github.com>
2023-03-22 10:20:26 -0700

    added cmments in js coding

--- block07/vault.js
+++ block07/vault.js
@@ -10,19 +10,22 @@
     Final pop-up with combo or success string
 */
 
+// First alert upon page opening
 alert(
   "You have received this message because you have been chosen to open an important vault. Please continue for combination!!"
 );
 
+// const used as variables for arithmetic operator
 const num1 = 5 + 5;
 alert(num1);
 
 const num2 = 4 * 10;
 alert(num2);
 
-const num3 = 39 % 100;
+const num3 = 100 - 61;
 alert(num3);
 
+//string alerts for combination
 let str1 = "10";
 let str2 = "40";
 let str3 = "39";
