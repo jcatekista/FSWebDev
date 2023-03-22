6a8baffb01c1d9a53c6d3ce4081dc26a28468d9c
jcatekista <jocateko@SEA-GP3N6GQ9XFN.ant.amazon.com>
2023-03-22 10:14:01 -0700

    block07 vault workshop

---
+++ block07/vault.js
@@ -0,0 +1,31 @@
+/* Steps for completing Vault Module
+    Create Pseudocode
+    Create or find vault gif to use
+    Create html file
+    Create javascript file
+    Add script line to html file
+    Create first alert upon page load
+    Alert message to say "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+    Create 3 variable lines to equal combination
+    Final pop-up with combo or success string
+*/
+
+alert(
+  "You have received this message because you have been chosen to open an important vault. Please continue for combination!!"
+);
+
+const num1 = 5 + 5;
+alert(num1);
+
+const num2 = 4 * 10;
+alert(num2);
+
+const num3 = 39 % 100;
+alert(num3);
+
+let str1 = "10";
+let str2 = "40";
+let str3 = "39";
+alert("Here is the secret combination");
+
+alert(str1 + " " + str2 + " " + str3);
